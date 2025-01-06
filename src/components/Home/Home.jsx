18d2cb0d9d7dfbb648f68e6141d4d13549ab406e
Mohd Afsar Hussain <140274258+afsar-hussai@users.noreply.github.com>
2025-01-06 20:11:07 +0530

    index heading and Home image changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -33,12 +33,12 @@ export default function Home() {
               </div>
 
               <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                  <img className="w-96" src="https://i.ibb.co/5BCcDYB/Remote2.png" alt="image1" />
+                  <img className="w-96" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCz2CtP5rYPeHW44RgBLf6vlY3URDA_JPyRQ&s" alt="image1" />
               </div>
           </aside>
 
           <div className="grid  place-items-center sm:mt-20">
-              <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+              <img className="sm:w-96 w-48" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCz2CtP5rYPeHW44RgBLf6vlY3URDA_JPyRQ&s" alt="image2" />
           </div>
 
           <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
